198f9daf299dd28f698321da157a2b44cbbdb195
EsleySaab <esleysantana.sa@gmail.com>
2024-11-07 14:41:20 -0300

    selecting a time

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -2,6 +2,8 @@ import dayjs from "dayjs"
 
 import { openingHours } from "../../utils/opening-hours.js"
 
+import { hoursClick } from "./hours-click.js"
+
 const hours = document.getElementById("hours")
 
 export function hoursLoad({ date }) {
@@ -26,6 +28,25 @@ export function hoursLoad({ date }) {
     li.classList.add(available ? "hour-available" : "hour-unavailable")
 
     li.textContent = hour
+
+    if (hour === "9:00") {
+      hourHeaderAdd("Manhã")
+    } else if (hour === "12:00") {
+      hourHeaderAdd("Tarde")
+    } else if (hour === "18:00") {
+      hourHeaderAdd("Noite")
+    }
     hours.append(li)
   })
+
+  // Adiciona o evento de clique nos horários disponíveis.
+  hoursClick()
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li")
+  header.classList.add("hour-period")
+  header.textContent = title
+
+  hours.append(header)
 }
